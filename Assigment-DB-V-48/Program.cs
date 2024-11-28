6d17beab8f323ba197c58225be08ead81b22e4d4
D-Naslund <daniel.naslund19@gmail.com>
2024-11-28 14:50:08 +0100

    Add project files.

---
+++ Assigment-DB-V-48/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigment_DB_V_48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
